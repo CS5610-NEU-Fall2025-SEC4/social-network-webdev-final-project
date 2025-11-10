ce58bc5cd9e93a711e86e3d672b86a90ec37a685
Preethi-23102000 <preethi.yennemadi@gmail.com>
2025-11-10 18:46:09 -0500

    connect login and register with backend

--- src/app/logIn/page.tsx
+++ src/app/logIn/page.tsx
@@ -1,7 +1,58 @@
 'use client'
+import { useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { FaEye } from 'react-icons/fa'
+import { LuEyeClosed } from 'react-icons/lu'
 
-export default function logIn() {
+export default function LogIn() {
+  const router = useRouter()
+
+  // form state
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  //state variables
+  const [errorMessage, setErrorMessage] = useState<string>('')
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
+  //handle change
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+    setErrorMessage('')
+  }
+
+  //login
+  const handleLogIn = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const res = await fetch('http://localhost:5000/users/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: formData.username,
+          password: formData.password,
+        }),
+      })
+
+      const data = await res.json()
+      if (res.ok) {
+        setErrorMessage('')
+        // alert(data.message || 'Log In successful!')
+        router.push('/home')
+      } else {
+        // alert(data.message || 'Log In failed. Invalid Username or Password.')
+        setErrorMessage(data.message || 'Invalid username or password.')
+      }
+    } catch (err) {
+      console.error(err)
+      alert('Something went wrong. Please try again later.')
+    }
+  }
+
   return (
     <div id="wdp-login-screen">
       <div className="min-h-screen flex flex-col justify-center items-center bg-white text-gray-900">
@@ -10,32 +61,56 @@ export default function logIn() {
           <p className="text-gray-600 text-lg">Please log in to continue</p>
         </div>
 
-        <div>
+        {errorMessage && <p className="text-red-600 text-sm mb-3 text-center">{errorMessage}</p>}
+        <form onSubmit={handleLogIn}>
           <div className="mb-4">
             <input
               type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              onBlur={() => {
+                if (!formData.username.trim()) alert('Username cannot be empty')
+              }}
               placeholder="Username"
-              className="px-4 py-3 border border-gray-300 rounded-lg"
+              className="px-4 py-3 border border-gray-300 rounded-lg w-100"
             />
           </div>
 
           <div className="mb-4">
-            <input
-              type="password"
-              placeholder="Password"
-              className="px-4 py-3 border border-gray-300 rounded-lg"
-            />
+            <div className="relative w-full sm:w-auto sm:min-w-[400px]">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                onBlur={() => {
+                  if (!formData.password.trim()) alert('Password cannot be empty')
+                }}
+                placeholder="Password"
+                className="px-4 py-3 border border-gray-300 rounded-lg w-100"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)} // toggle button
+                className="absolute right-2 top-1/2 -translate-y-1/2 px-3 py-[6px] rounded-md bg-gray-100 hover:bg-gray-200 text-sm"
+              >
+                {showPassword ? <LuEyeClosed /> : <FaEye />}
+              </button>
+            </div>
           </div>
 
           <div className="text-center">
-            <Link
-              href="/home"
-              className="block bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl text-center no-underline"
-            >
-              Log In
-            </Link>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl text-center w-64"
+              >
+                Log In
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
         <div className="text-center mt-6">
           <Link
             href="./register"
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-4 md:p-6">
+      <h2 className="text-2xl font-semibold mt-1 mb-3 text-gray-800 ps-3">
+        Placeholder Profile Page - Under Construction
+      </h2>
+    </div>
+  )
+}
